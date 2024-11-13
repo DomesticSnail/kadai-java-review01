398cab77da3a7c36e8a7070782e8d886dbf5bf3d
DomesticSnail <philliptowler94@gmail.com>
2024-11-13 22:30:03 +0900

    Javaプログラミング入門学習終了

---
+++ src/Test.java
@@ -0,0 +1,6 @@
+public  class  Test { public static void main( String [] args) {
+         System .out.println( " TechAcademy " );
+         System .out.println( " Java " );
+         System .out.println( " Let's start learning! " ); 
+    } 
+}
---
+++ src/Usevariable.java
@@ -0,0 +1,12 @@
+
+public class Usevariable {
+
+    public static void main(String[] args) {
+        int number1 = 100;
+        String str1 = "xyz";
+        System.out.println(number1);
+        System.out.println(str1);
+
+    }
+
+}
