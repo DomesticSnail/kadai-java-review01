2ed97ad5ee4cb0b5cded84408aa272753eb3a24a
DomesticSnail <philliptowler94@gmail.com>
2024-11-14 16:37:57 +0900

    ２回目

--- src/Review01.java
+++ src/Review01.java
@@ -2,19 +2,15 @@ public class Review01 {
 
     public static void main(String[] args) {
         int mainPrice = 500;
-        int taxAmount = calculate(mainPrice);
+        int taxAmount = tax(mainPrice);
         int totalPrice = mainPrice + taxAmount;
 
         String output = mainPrice + "円の商品の税込価格は" + totalPrice + "円（消費税は" + taxAmount + "円）です。";
 
-        System.out.println("Main Price: " + mainPrice + "円");
-        System.out.println("Tax Amount: " + taxAmount + "円");
-        System.out.println("Total Price: " + totalPrice + "円");
         System.out.println(output);
     }
 
-
-    public static int calculate(int price) {
+    public static int tax(int price) {
         double taxRate = 0.1;
         return (int)(price * taxRate);
     }
